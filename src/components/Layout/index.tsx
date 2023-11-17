47d1263e6edb27844e82015d2d70f0ebecfd22d0
Lisofffa <lisofffa@gmail.com>
2023-11-17 15:50:34 +0300

    fix: changed styles to adaptive

--- src/components/Layout/index.tsx
+++ src/components/Layout/index.tsx
@@ -12,7 +12,7 @@ export const Layout: FC<LayoutProps> = ({ children }) => {
   return (
     <div
       className={
-        'min-h-screen bg-dpl-white dark:backgroundDark dark:bg-dpl-black max-xl:overflow-x-hidden'
+        'min-h-screen bg-dpl-white dark:backgroundDark dark:bg-dpl-black xl:overflow-x-hidden max-xl:overflow-x-hidden'
       }
     >
       <Header />
