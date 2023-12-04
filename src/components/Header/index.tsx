ca01068d422f014ba22a180829a37f94aba267b8
Lisofffa <lisofffa@gmail.com>
2023-12-04 18:41:55 +0300

    fix: changed styles links for mobile menu

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -58,7 +58,7 @@ export function Header() {
             className={cn(
               styles.linkHover,
               {
-                [styles.active]: router.asPath === '/',
+                [styles.active]: router.asPath !== '/about',
               },
               'text-base '
             )}
@@ -156,7 +156,7 @@ export function Header() {
                   className={cn(
                     styles.linkHover,
                     {
-                      [styles.active]: router.asPath === '/',
+                      [styles.active]: router.asPath !== '/about',
                     },
                     'text-base '
                   )}
