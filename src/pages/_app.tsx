1988c49dd80d566db70757278d1c659f9b0ae7d6
Alexander Sakhaev <alsakhaev@gmail.com>
2023-12-05 00:14:12 +0100

    feat: matomo analytics

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,10 +1,22 @@
 import type { AppProps } from 'next/app';
-import { FC } from 'react';
+import { FC, useEffect } from 'react';
 import { ThemeProvider } from 'next-themes';
+import init from '@socialgouv/matomo-next';
 
 import '@/assets/styles/globals.css';
 
+const { MATOMO_URL, MATOMO_SITE_ID } = process.env;
+
 const MyApp: FC<AppProps> = ({ Component, ...rest }) => {
+  // Initialize Matomo Analytics
+  useEffect(() => {
+    if (!MATOMO_URL || !MATOMO_SITE_ID) return;
+    init({
+      url: MATOMO_URL,
+      siteId: MATOMO_SITE_ID,
+    });
+  }, []);
+
   return (
     <ThemeProvider
       attribute='class'
